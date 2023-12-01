5fb308bd49ef4230c0750b59ed354be97e0edc0a
Tania <113130993+Tania66@users.noreply.github.com>
2023-12-01 14:00:39 +0200

    home-work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,81 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+import shortid from 'shortid';
+import css from './phonebook/Style.module.css'
+import ContactForm from './phonebook/ContactForm/ContactForm';
+import Filter from './phonebook/Filter/Filter';
+import ContactList from './phonebook/ContactList/ContactList ';
+
+
+
+class App extends React.Component {
+  state = {
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+    filter: ''
+  };
+
+
+
+  addContact = ({name, number}) =>{
+const contact = {
+  id:shortid.generate(),
+  name:name,
+  number:number
 };
+
+if (contact) {
+  alert(`${contact.name} is already in contacts`);
+  return;
+}
+
+this.setState(prevState => ({
+  contacts: [contact, ...prevState.contacts],
+}))
+
+  }
+
+
+onChangeFilter = (event) => {
+this.setState({filter:event.currentTarget.value})
+}
+
+// getFilterName = () => {
+//   const normalizeNameFilter = this.state.filter.toLowerCase();
+//   return this.state.contacts.filter(contact => 
+//     contact.name.toLowerCase().includes(normalizeNameFilter))
+// }
+
+deleteContact = contactId =>{
+this.setState(prevState => ({
+  contacts: prevState.contacts.filter(contact => contact.id !== contactId)
+}))
+}
+
+
+  render() {
+
+const normalizeNameFilter = this.state.filter.toLowerCase();
+
+const filterContacts = this.state.contacts.filter(contact => 
+  contact.name.toLowerCase().includes(normalizeNameFilter))
+
+
+
+    return (
+      <div className={css.inner}>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm  onSubmit={this.addContact}/>
+
+        <h2 className={css.title}>Contacts</h2>
+        <Filter value={this.state.filter} onChange={this.onChangeFilter}/>
+        <ContactList className={css.list} contacts={filterContacts}  onDeleteContact={this.deleteContact}/>
+      </div>
+    );
+  }
+}
+
+export default App;
