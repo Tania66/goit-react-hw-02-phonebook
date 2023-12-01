673ff19e272cdcc567205af027eda202eb0bfe8f
Tania <113130993+Tania66@users.noreply.github.com>
2023-12-01 22:44:59 +0200

    fix savedContact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,13 +28,20 @@ const contact = {
 };
 
 const saveContact = this.state.contacts.some(contact =>
-  contact.name.toLowerCase() === name.toLowerCase(), contact.number === number)
+  contact.name.toLowerCase() === name.toLowerCase())
 
   if (saveContact) {
     alert(`${contact.name} is already in contacts`)
     return
   }
-  
+
+  const saveNumber = this.state.contacts.some(contact =>
+    contact.number === number)
+
+    if (saveNumber) {
+      alert(`${contact.number} is already in contacts`)
+      return
+    }
 
 this.setState(prevState => ({
   contacts: [contact, ...prevState.contacts],
